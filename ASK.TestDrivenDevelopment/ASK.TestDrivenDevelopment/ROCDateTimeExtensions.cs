fbce798987372450307d0262b53cf95978584802
CHC <howjerry22@gmail.com>
2022-04-12 21:06:36 +0800

    * add test Driven class

---
+++ ASK.TestDrivenDevelopment/ASK.TestDrivenDevelopment/ROCDateTimeExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASK.TestDrivenDevelopment
+{
+    public static class ROCDateTimeExtensions
+    {
+
+        public static string UtcDate(this DateTime target)
+        {
+            var result = string.Empty;
+            return result;
+        }
+
+
+        public static string LittleBitDate(this DateTime target)
+        {
+            var result = string.Empty;
+            return result;
+        }
+
+
+        public static string ShortDate(this DateTime target)
+        {
+            var result = string.Empty;
+            return result;
+        }
+
+
+        public static string LognData(this DateTime target)
+        {
+            var result = string.Empty;
+            return result;
+        }
+
+
+        public static string LongDateAndWeek(this DateTime target)
+        {
+            var result = string.Empty;
+            return result;
+        }
+
+
+
+        public static string LongDateAndLongWeek(this DateTime target)
+        {
+            var result = string.Empty;
+            return result;
+        }
+    }
+}
---
+++ ASK.TestDrivenDevelopment/ASK.UnitTesting/UnitTest1.cs
@@ -0,0 +1,68 @@
+using ASK.TestDrivenDevelopment;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ASK.UnitTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private TestContext testContextInstance;
+        public TestContext TestContext
+        {
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
+        }
+
+        [TestMethod]
+        public void UtcDate()
+        {
+            var date = new DateTime(2012, 02, 29).ToUniversalTime();
+
+            Assert.AreEqual($"101.02.29", date.UtcDate());
+        }
+
+        [TestMethod]
+        public void LittleBitDate()
+        {
+            var date = new DateTime(2012, 02, 29);
+
+            Assert.AreEqual($"101.02.29", date.LittleBitDate());
+        }
+
+        [TestMethod]
+        public void ShortDate()
+        {
+            var date = new DateTime(2012, 02, 29);
+
+            Assert.AreEqual($"101年02月29日", date.ShortDate());
+        }
+
+        [TestMethod]
+        public void LognData()
+        {
+            var date = new DateTime(2012, 02, 29);
+
+            Assert.AreEqual($"民國101年02月29日", date.LognData());
+        }
+
+        [TestMethod]
+        public void LongDateAndWeek()
+        {
+            var date = new DateTime(2012, 02, 29);
+
+
+            Assert.AreEqual("民國101年02月29日 週三", date.LongDateAndWeek());
+        }
+
+
+        [TestMethod]
+        public void LongDateAndLongWeek()
+        {
+            var date = new DateTime(2012, 02, 29);
+            
+
+            Assert.AreEqual("民國101年02月29日 星期三", date.LongDateAndLongWeek());
+        }
+    }
+}
