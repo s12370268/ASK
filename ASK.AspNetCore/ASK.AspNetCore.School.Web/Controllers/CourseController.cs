7fde7a19cfefd0291421d9b187283a2ee5a02b36
CHC <howjerry22@gmail.com>
2022-04-17 22:42:29 +0800

    * 檢視頁

---
+++ ASK.AspNetCore/ASK.AspNetCore.School.Web/Controllers/CourseController.cs
@@ -0,0 +1,51 @@
+﻿using ASK.AspNetCore.School.Web.Datas;
+using ASK.AspNetCore.School.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASK.AspNetCore.School.Web.Controllers
+{
+    public class CourseController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(CourseRepository.GetList());
+        }
+
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            return View(CourseRepository.Get(1));
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Course model)
+        {
+            if(ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return View(CourseRepository.Get(1));
+        }
+
+        [HttpGet]
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        [HttpGet]
+
+        public IActionResult Delete()
+        {
+            return RedirectToAction("Index");
+        }
+
+        public string GetMessage()
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ ASK.AspNetCore/ASK.AspNetCore.School.Web/Datas/StudentRepository.cs
@@ -0,0 +1,33 @@
+﻿using ASK.AspNetCore.School.Web.Models;
+
+namespace ASK.AspNetCore.School.Web.Datas
+{
+    public class StudentRepository
+    {
+        public static Student Get(string id)
+        {
+            Student entity = new()
+            {
+                 ID = "A123456789",
+                 Name = "好學生"
+            };
+
+
+            return entity;
+        }
+
+
+        public static IEnumerable<Student> GetList()
+        {
+            var entities = new List<Student>();
+            entities.Add(new() { ID = "A123456789", Name = "好學生" });
+            entities.Add(new() { ID = "H123446565", Name = "楊慶鈺" });
+            entities.Add(new() { ID = "T123456141", Name = "賴喜添" });
+            entities.Add(new() { ID = "Y223114789", Name = "王雅昌" });
+            entities.Add(new() { ID = "E123456789", Name = "廣玉婷" });
+            entities.Add(new() { ID = "G123456789", Name = "陳婷婷" });
+            entities.Add(new() { ID = "G123558767", Name = "簡佳玲" });
+            return entities;
+        }
+    }
+}
